c44b4261fa58b458c9202ffb0531da121347f265
Konrad Klimczak <konrad.klimczak@vistex.com>
2017-10-02 18:04:57 +0200

    First lesson.

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,24 @@
 const { Component } = React;
 
+const neverUpdate = (BaseComponent) =>
+  class extends Component {
+    shouldComponentUpdate() {
+      return false;
+    }
+    render() {
+      return <BaseComponent {...this.props} />;
+    }
+  }
+
 const User = ({ name }) =>
   <div className="User">{ name }</div>;
 
+const User2 = neverUpdate(User);
 
 const App = () =>
   <div>
     <User name="Tim" />
+    <User2 name="joe" />
   </div>;
 
 ReactDOM.render(
